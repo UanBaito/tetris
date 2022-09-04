08ec16b416ee0c0dfa2a3c3b982a8ee2230b9620
UanBaito <97982506+UanBaito@users.noreply.github.com>
2022-09-04 13:46:44 -0400

    Add getTetrominoPoints function

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { tetromino } from '../interfaces';
 import GameBox from './GameBox';
 export default function Game() {
 	/**
@@ -14,35 +15,37 @@ export default function Game() {
 	/**
 	 * This is the tetromino currently controlled by the player
 	 */
-	const [currentTetrominoState, setCurrentTetrominoState] = useState({
-		shape: 'L',
-		color: 'blue',
-		facing: 0, ///Current rotation, 0 = up, 1 = right, 2 = down, 3 = left.
-		coords: {
-			/**
-			 * This is the position of the square that the other squares will rotate around.
-			 * When this property is updated, the other squares will be mapped around it.
-			 * This could be considered the "true" position of the entire tetromino.
-			 */
-			axis: {
-				x: 4,
-				y: 1
-			},
-			/**
-			 * The shapeCoords property tells where the squares around the axis are
-			 * depending on the rotation, describing the shape of the tetromino
-			 */
-			shapeCoords: {
-				facingUpPoints: [
-					///(x, y)
-					[0, 0],
-					[-1, 0],
-					[1, 0],
-					[-1, -1]
-				]
+	const [currentTetrominoState, setCurrentTetrominoState] = useState<tetromino>(
+		{
+			shape: 'L',
+			color: 'blue',
+			facing: 0, ///Current rotation, 0 = up, 1 = right, 2 = down, 3 = left.
+			coords: {
+				/**
+				 * This is the position of the square that the other squares will rotate around.
+				 * When this property is updated, the other squares will be mapped around it.
+				 * This could be considered the "true" position of the entire tetromino.
+				 */
+				axis: {
+					x: 4,
+					y: 1
+				},
+				/**
+				 * The shapeCoords property tells where the squares around the axis are
+				 * depending on the rotation, describing the shape of the tetromino
+				 */
+				shapeCoords: {
+					facingUpPoints: [
+						///(x, y)
+						[0, 0],
+						[-1, 0],
+						[1, 0],
+						[-1, -1]
+					]
+				}
 			}
 		}
-	});
+	);
 
 	/**
 	 * When  gameState is true, this useEffect basically makes the game tick and
@@ -65,6 +68,38 @@ export default function Game() {
 	 */
 	function startTimer() {
 		setgameState(true);
+	}
+
+	function getTetrominoPoints() {
+		const axis = currentTetrominoState.coords.axis;
+		const rotation = currentTetrominoState.facing;
+		let relativePoints!: number[][];
+		switch (rotation) {
+			case 0:
+				relativePoints =
+					currentTetrominoState.coords.shapeCoords.facingUpPoints;
+				break;
+			case 1:
+				relativePoints =
+					currentTetrominoState.coords.shapeCoords.facingRightPoints!;
+				break;
+			case 2:
+				relativePoints =
+					currentTetrominoState.coords.shapeCoords.facingDownPoints!;
+				break;
+			case 3:
+				relativePoints =
+					currentTetrominoState.coords.shapeCoords.facingLeftPoints!;
+				break;
+		}
+		const TetrominoPoints = relativePoints.map((point) => {
+			const newPoint: number[] = [];
+			const x = point[0] + axis.x;
+			const y = point[1] + axis.y;
+			newPoint.push(x, y);
+			return newPoint;
+		});
+		return TetrominoPoints;
 	}
 
 	return (
