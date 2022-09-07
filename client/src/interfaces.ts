3cc66896cc595b72fbbf9b4b835ce3dce44f949b
UanBaito <97982506+UanBaito@users.noreply.github.com>
2022-09-06 21:22:18 -0400

    Rea moved tetrominoes comments to interface

--- client/src/interfaces.ts
+++ client/src/interfaces.ts
@@ -1,14 +1,24 @@
 export interface tetromino {
 	shape: string;
 	color: string;
-	facing: number;
+	facing: number; ///Current rotation, 0 = up, 1 = right, 2 = down, 3 = left.
 	moving: boolean;
 	coords: {
+		/**
+		 * This is the position of the square that the other squares will rotate around.
+		 * When this property is updated, the other squares will be mapped around it.
+		 * This could be considered the "true" position of the entire tetromino.
+		 */
 		axis: {
 			x: number;
 			y: number;
 		};
+		/**
+		 * The shapeCoords property tells where the squares around the axis are
+		 * depending on the rotation, describing the shape of the tetromino
+		 */
 		shapeCoords: {
+			///(x, y)
 			facingUpPoints: Array<Array<number>>;
 			facingRightPoints?: Array<Array<number>>;
 			facingDownPoints?: Array<Array<number>>;
