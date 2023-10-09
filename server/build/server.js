bd42f5729b51adbcc9d20f743f0784d2ddae91ec
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-09 12:09:12 -0400

    basic communication between client and server

--- server/build/server.js
+++ server/build/server.js
@@ -3,56 +3,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("./config/config");
 const express_1 = __importDefault(require("express"));
-const http_1 = __importDefault(require("http"));
-const mongoose_1 = __importDefault(require("mongoose"));
-const Logging_1 = __importDefault(require("./library/Logging"));
-const User_1 = __importDefault(require("./routes/User"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
-/* Connect to mongoose */
-mongoose_1.default
-    .connect(config_1.config.mongo.url, { retryWrites: true, w: 'majority' })
-    .then(() => {
-        Logging_1.default.info('Connected to MongoDB');
-        StartServer();
-    })
-    .catch((error) => {
-        Logging_1.default.error('Unable to connect');
-        Logging_1.default.error(error);
-    });
-/** Only start the server if Mongo connects */
-const StartServer = () => {
-    app.use((req, res, next) => {
-        /**Log the request */
-        Logging_1.default.info(`Incoming -> Method: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
-        res.on('finish', () => {
-            `Outgoing -> Method: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] Status: [${res.statusCode}]`;
-        });
-        next();
-    });
-    app.use(express_1.default.urlencoded({ extended: true }));
-    /** Rules of our API */
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
-        if (req.method == 'OPTIONS') {
-            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-            return res.status(200).json({});
-        }
-        next();
-    });
-    /**Routes */
-    app.use('/users', User_1.default);
-    /** Healthcheck */
-    app.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong' }));
-    /** Error handling */
-    app.use((req, res, next) => {
-        const error = new Error('not found');
-        Logging_1.default.error(error);
-        return res.status(404).json({ message: error.message });
-    });
-    http_1.default
-        .createServer(app)
-        .listen(config_1.config.server.port, () => Logging_1.default.info(`Server is running on port ${config_1.config.server.port}`));
-};
+const port = 9001;
+app.use((0, cors_1.default)({ origin: 'http://localhost:5173' }));
+app.use(body_parser_1.default.json());
+app.post('/user', (req, res) => {
+    if (req.body.name === '') {
+        res.statusCode = 400;
+        res.send('name cannot be empty');
+        console.log('name cannot be empty');
+    }
+    else {
+        res.statusCode = 200;
+        res.send('request to post ' + req.body + ' received');
+        console.log('request to post ' + req.body + ' received');
+    }
+});
+const users = [];
+app.get('/', (req, res) => {
+    console.log('elpepe');
+    res.send('hello');
+});
+function addUser(name, score) { }
+app.listen(port, () => {
+    console.log('listening on port ' + port);
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,38 @@
 import express from 'express';
-import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
+const port = 9001;
+
+type user = {
+	name: string;
+};
+
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(bodyParser.json());
+
+app.post('/user', (req, res) => {
+	if (req.body.name === '') {
+		res.statusCode = 400;
+		res.send('name cannot be empty');
+		console.log('name cannot be empty');
+	} else {
+		res.statusCode = 200;
+		res.send('request to post ' + req.body + ' received');
+		console.log('request to post ' + req.body + ' received');
+	}
+});
+
+const users: user[] = [];
+
+app.get('/', (req, res) => {
+	console.log('elpepe');
+	res.send('hello');
+});
+
+function addUser(name: string, score: number) {}
+
+app.listen(port, () => {
+	console.log('listening on port ' + port);
+});
